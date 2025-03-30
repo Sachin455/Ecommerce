fed039ff4769a8f8d5ad23b6c01aed75bf6b661a
sachin455 <karkisachin455@gmail.com>
2025-03-30 12:47:16 +0545

    initial commit

---
+++ config/inngest.js
@@ -0,0 +1,48 @@
+import { Inngest } from "inngest";
+import connectDB from "./db";
+import User from "@/models/User";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "quickcart-next" });
+
+// Ingest Function to save user data to database
+export const syncUserCreation = inngest.createFunction(
+    {
+        id: 'sync-user-from-clerk'
+    },
+    {
+        event:'clerk/user.created'
+    },
+    async ({event}) => {
+        const{ id, first_name, last_name, email_addresses, image_url } = event.data
+        const userData = {
+            _id:id,
+            email:email_addresses[0].email_addresses,
+            name: first_name + ' ' + last_name,
+            imageUrl: image_url
+
+        }
+        await connectDB() 
+        await User.create(userData)
+
+    }
+)
+
+//Inngest function to update user data to database
+export const syncUserUpdation = inngest.createFunction(
+    {
+        id: 'update-user-from-clerk'
+    },
+    {event: 'clerk/user.updated'},
+    async ({event}) => {
+        const{ id, first_name, last_name, email_addresses, image_url } = event.data
+        const userData = {
+            _id:id,
+            email:email_addresses[0].email_addresses,
+            name: first_name + ' ' + last_name,
+            imageUrl: image_url
+
+        }
+        await connectDB() 
+        await User.findByIdAndUpdate(id,userData)
+)
